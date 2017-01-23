8f1d117c6552d90d0faff86ca63b3d1f4e318f17
Giorgos Katsikis <katsikhs@gmail.com>
2017-01-23 13:39:49 +0200

    Testing

--- elot743.js
+++ elot743.js
@@ -34,8 +34,6 @@ window.onload = function() {
           {greek:'εύ', fivi:1},
           {greek:'ηυ', fivi:1},
           {greek:'ηύ', fivi:1},
-          {greek:'ευα', fiva:1},
-          {greek:'ευά', fiva:1},
           {greek:'ντ', greeklish:'nt'},
           {greek:'μπ', bi:1},
           {greek:'τσ', greeklish:'ts'},
@@ -77,11 +75,6 @@ window.onload = function() {
           if (replacement.bi) {
             var bi = (greekSet[text.charAt(index - 1).toLowerCase()] && greekSet[text.charAt(index + 2).toLowerCase()]) ? 'mp' : 'b'
             return fixCase(bi, $0)
-          } else if (replacement.fiva) {
-            var c1 = replacements[$0.charAt(0).toLowerCase()].greeklish
-            var c2 = vaSet[text.charAt(index + 2).toLowerCase()] ? 'f' : 'v'
-            var c3 = replacements[$0.charAt(2).toLowerCase()].greeklish
-            return fixCase(c1 + c2 + c3, $0)
           } else if (replacement.fivi) {
             var c1 = replacements[$0.charAt(0).toLowerCase()].greeklish
             var c2 = viSet[text.charAt(index + 2).toLowerCase()] ? 'v' : 'f'
