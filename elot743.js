0e129a1874eb1a25b4b75e9c687304646862a3c7
Giorgos Katsikis <katsikhs@gmail.com>
2017-01-23 13:45:21 +0200

    Testing

--- elot743.js
+++ elot743.js
@@ -77,7 +77,7 @@ window.onload = function() {
             var bi = (greekSet[text.charAt(index - 1).toLowerCase()] && greekSet[text.charAt(index + 2).toLowerCase()]) ? 'mp' : 'b'
             return fixCase(bi, $0)
             else if (replacement.eva) {
-            var eva = (greekSet[text.charAt(index - 1).toLowerCase()] && greekSet[text.charAt(index + 2).toLowerCase()]) ? 'mp' : 'b'
+            var eva = (greekSet[text.charAt(index - 1).toLowerCase()] && greekSet[text.charAt(index + 2).toLowerCase()]) ? 'f' : 'v'
             return fixCase(eva, $0)
           } else if (replacement.fivi) {
             var c1 = replacements[$0.charAt(0).toLowerCase()].greeklish
