c153b66281a62443626cff76bd002296c0a764f0
Giorgos Katsikis <katsikhs@gmail.com>
2017-01-23 13:10:52 +0200

    Test

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+<!--
+	Eventually by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Greek to ELOT743</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+	</head>
+	<body>
+<script type="text/javascript" src="http://helppost.gr/wp-content/script-our/converter-elot-743.js"></script>
+<h2><textarea name="input" id="input"></textarea></h2>
+<h2><textarea id="output"></textarea></h2>
+
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
